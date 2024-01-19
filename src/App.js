c239d0fd811c6686789b87686a7efaab8a437ced
choiinyoung <choi121100@naver.com>
2024-01-19 17:50:15 +0900

    Link 설정
    Link 설정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Available from "./component/Available";
 import Header from "./component/Header";
@@ -5,12 +6,30 @@ import Home from "./component/Home";
 import Profile from "./component/Profile";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Profile />
-      <Available />
-    </div>
+    // <div className="App">
+    //   <Router>
+    //     <Header />
+
+    //     {/* <Home />
+    //   <Profile />
+    //   <Available /> */}
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/profile" element={<Profile />} />
+    //       <Route path="/available" element={<Available />} />
+    //     </Routes>
+    //   </Router>
+    // </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/available" element={<Available />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "../style/header.scss";
+import { Link } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Available from "./Available";
 
 export default function Header() {
   return (
-    <div className="header">
-      <p className="title">Front-end & Web Developer</p>
-      <ul>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Available</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <>
+      <div className="header">
+        <p className="title">Front-end & Web Developer</p>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/available">Available</Link>
+        </nav>
+        {/* <li>Portfolio</li>
+        <li>Contact</li> */}
+      </div>
+    </>
   );
 }
