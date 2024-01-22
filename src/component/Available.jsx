3d8e4c929fbc55fca8823080fe8fe7e9d8d1070f
choiinyoung <choi121100@naver.com>
2024-01-23 02:03:10 +0900

    Available.jsx_Doughnut차트
    Doughnut

--- src/component/Available.jsx
+++ src/component/Available.jsx
@@ -7,7 +7,57 @@ import jquery_img from "../img/jquery.png";
 import js_img from "../img/javascript.png";
 import ts_img from "../img/typescript.png";
 import react_img from "../img/react.png";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement } from "chart.js";
+
+ChartJS.register(ArcElement);
+
 export default function Available() {
+  const Data1 = {
+    datasets: [
+      {
+        data: [90, 10],
+        backgroundColor: ["orange", "#ffffff00"],
+        borderWidth: 0,
+        borderRadius: 30,
+      },
+    ],
+  };
+
+  const Data2 = {
+    datasets: [
+      {
+        data: [80, 20],
+        backgroundColor: ["orange", "#ffffff00"],
+        borderWidth: 0,
+        borderRadius: 30,
+      },
+    ],
+  };
+  const Data3 = {
+    datasets: [
+      {
+        data: [70, 30],
+        backgroundColor: ["orange", "#ffffff00"],
+        borderWidth: 0,
+        borderRadius: 30,
+      },
+    ],
+  };
+  const Data4 = {
+    datasets: [
+      {
+        data: [40, 60],
+        backgroundColor: ["orange", "#ffffff00"],
+        borderWidth: 0,
+        borderRadius: 30,
+      },
+    ],
+  };
+  const Options = {
+    cutout: "80%", // 도넛 차트에서 반만 채우도록 cutout 옵션 설정
+  };
+
   return (
     <div className="available">
       <div className="front">
@@ -16,40 +66,33 @@ export default function Available() {
         <div className="wave2 circle2"></div>
         <div className="front_img">
           <div className="pie-chart pic-chart1">
-            <span className="center">
-              <img src={html_img} alt="html_img" />
-            </span>
+            <Doughnut data={Data1} options={Options}></Doughnut>
+            <img src={html_img} alt="html_img" />
           </div>
-          {/* <div className="pie-chart pic-chart2">
-            <span className="center">
-              <img src={css_img} alt="css_img" />
-            </span>
+          <div className="pie-chart pic-chart2">
+            <Doughnut data={Data1} options={Options}></Doughnut>
+            <img src={css_img} alt="css_img" />
           </div>
           <div className="pie-chart pic-chart3">
-            <span className="center">
-              <img src={sass_img} alt="sass_img" />
-            </span>
+            <Doughnut data={Data2} options={Options}></Doughnut>
+            <img src={sass_img} alt="sass_img" />
           </div>
           <div className="pie-chart pic-chart4">
-            <span className="center">
-              <img src={jquery_img} alt="jquery_img" />
-            </span>
+            <Doughnut data={Data2} options={Options}></Doughnut>
+            <img src={jquery_img} alt="jquery_img" />
           </div>
           <div className="pie-chart pic-chart5">
-            <span className="center">
-              <img src={js_img} alt="js_img" />
-            </span>
+            <Doughnut data={Data3} options={Options}></Doughnut>
+            <img src={js_img} alt="js_img" />
           </div>
           <div className="pie-chart pic-chart6">
-            <span className="center">
-              <img src={ts_img} alt="ts_img" />
-            </span>
+            <Doughnut data={Data4} options={Options}></Doughnut>
+            <img src={ts_img} alt="ts_img" />
           </div>
           <div className="pie-chart pic-chart7">
-            <span className="center">
-              <img src={react_img} alt="react_img" />
-            </span>
-          </div> */}
+            <Doughnut data={Data3} options={Options}></Doughnut>
+            <img src={react_img} alt="react_img" />
+          </div>
         </div>
       </div>
       <div className="etc">
