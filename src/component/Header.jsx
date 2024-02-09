b08eb829b044aac91fb36f87b41d9ec2be9ca72a
choiinyoung <choi121100@naver.com>
2024-02-09 13:35:10 +0900

    header수정
    scroll할 때 header위치 변경

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,74 +1,168 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/header.scss";
 import { Link } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faCode,
+  faFileCode,
+  faPenNib,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-  return (
-    <>
-      <div className="header">
-        <ul>
-          <li>
-            <Link
-              activeClass="active"
-              to="home"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={1000}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="profile"
-              spy={true}
-              smooth={true}
-              offset={45}
-              duration={1000}
-            >
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="available"
-              spy={true}
-              smooth={true}
-              offset={48}
-              duration={1000}
-            >
-              Available
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="portfolio1"
-              spy={true}
-              smooth={true}
-              offset={49}
-              duration={1000}
-            >
-              Portfolio#1
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="portfolio2"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={1000}
-            >
-              Portfolio#2
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+  const [scrollY, setScrollY] = useState(window.scrollY);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const renderHeader = () => {
+    switch (true) {
+      case scrollY >= 900:
+        return (
+          <div className="left">
+            <ul className="header_top">
+              <li>
+                <Link
+                  activeClass="active"
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1000}
+                >
+                  <FontAwesomeIcon icon={faHouse} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="profile"
+                  spy={true}
+                  smooth={true}
+                  offset={45}
+                  duration={1000}
+                >
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="available"
+                  spy={true}
+                  smooth={true}
+                  offset={48}
+                  duration={1000}
+                >
+                  <FontAwesomeIcon icon={faCode} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="portfolio1"
+                  spy={true}
+                  smooth={true}
+                  offset={49}
+                  duration={1000}
+                >
+                  <FontAwesomeIcon icon={faFileCode} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="portfolio2"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={1000}
+                >
+                  <FontAwesomeIcon icon={faPenNib} />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        );
+      default:
+        return (
+          <div className="header">
+            <ul className="header_top">
+              <li>
+                <Link
+                  activeClass="active"
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1000}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="profile"
+                  spy={true}
+                  smooth={true}
+                  offset={45}
+                  duration={1000}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="available"
+                  spy={true}
+                  smooth={true}
+                  offset={48}
+                  duration={1000}
+                >
+                  Available
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="portfolio1"
+                  spy={true}
+                  smooth={true}
+                  offset={49}
+                  duration={1000}
+                >
+                  Portfolio#1
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="portfolio2"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={1000}
+                >
+                  Portfolio#2
+                </Link>
+              </li>
+            </ul>
+          </div>
+        );
+    }
+  };
+
+  return renderHeader();
 }
