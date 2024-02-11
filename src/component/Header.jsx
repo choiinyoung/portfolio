59a5380c9dba2615a6a92730aca560b53d9cd6b1
choiinyoung <choi121100@naver.com>
2024-02-11 16:25:20 +0900

    header이미지로 수정
    header이미지로 수정

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../style/header.scss";
 import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,158 +11,75 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-  const [scrollY, setScrollY] = useState(window.scrollY);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const renderHeader = () => {
-    switch (true) {
-      case scrollY >= 900:
-        return (
-          <div className="left">
-            <ul className="header_top">
-              <li>
-                <Link
-                  activeClass="active"
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={1000}
-                >
-                  <FontAwesomeIcon icon={faHouse} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="profile"
-                  spy={true}
-                  smooth={true}
-                  offset={45}
-                  duration={1000}
-                >
-                  <FontAwesomeIcon icon={faUser} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="available"
-                  spy={true}
-                  smooth={true}
-                  offset={48}
-                  duration={1000}
-                >
-                  <FontAwesomeIcon icon={faCode} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="portfolio1"
-                  spy={true}
-                  smooth={true}
-                  offset={49}
-                  duration={1000}
-                >
-                  <FontAwesomeIcon icon={faFileCode} />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="portfolio2"
-                  spy={true}
-                  smooth={true}
-                  offset={50}
-                  duration={1000}
-                >
-                  <FontAwesomeIcon icon={faPenNib} />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        );
-      default:
-        return (
-          <div className="header">
-            <ul className="header_top">
-              <li>
-                <Link
-                  activeClass="active"
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={1000}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="profile"
-                  spy={true}
-                  smooth={true}
-                  offset={45}
-                  duration={1000}
-                >
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="available"
-                  spy={true}
-                  smooth={true}
-                  offset={48}
-                  duration={1000}
-                >
-                  Available
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="portfolio1"
-                  spy={true}
-                  smooth={true}
-                  offset={49}
-                  duration={1000}
-                >
-                  Portfolio#1
-                </Link>
-              </li>
-              <li>
-                <Link
-                  activeClass="active"
-                  to="portfolio2"
-                  spy={true}
-                  smooth={true}
-                  offset={50}
-                  duration={1000}
-                >
-                  Portfolio#2
-                </Link>
-              </li>
-            </ul>
-          </div>
-        );
-    }
-  };
-
-  return renderHeader();
+  return (
+    <div className="header">
+      <ul className="header_top">
+        <li>
+          <Link
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={1000}
+            className="nav-link"
+          >
+            <FontAwesomeIcon icon={faHouse} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="profile"
+            spy={true}
+            smooth={true}
+            offset={45}
+            duration={1000}
+            className="nav-link"
+          >
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="available"
+            spy={true}
+            smooth={true}
+            offset={48}
+            duration={1000}
+            className="nav-link"
+          >
+            <FontAwesomeIcon icon={faCode} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="portfolio1"
+            spy={true}
+            smooth={true}
+            offset={49}
+            duration={1000}
+            className="nav-link"
+          >
+            <FontAwesomeIcon icon={faFileCode} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="portfolio2"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={1000}
+            className="nav-link"
+          >
+            <FontAwesomeIcon icon={faPenNib} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
